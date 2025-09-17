855ddb890e96b73017ecfd4fdf7b1d65172d8e46
serge-plourde <serge.g.plourde@gmail.com>
2025-09-17 13:18:15 -0400

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// These values come from GitHub Secrets at build time
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
